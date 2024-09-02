13d7ce8bc53392059995f24f25e787ce7dacf6ba
YeoSungeun <dutjddms@naver.com>
2024-09-02 19:51:38 +0900

    Initial Commit

---
+++ SwfitUIPracticeProject/SwfitUIPracticeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwfitUIPracticeProjectApp.swift
+//  SwfitUIPracticeProject
+//
+//  Created by 여성은 on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwfitUIPracticeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
